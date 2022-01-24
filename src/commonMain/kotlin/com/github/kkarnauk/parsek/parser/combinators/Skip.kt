e61cdf35deb6bdad0c7502e808431274a4665e51
Kirill <bitikit@gmail.com>
2022-01-25 00:44:30 +0300

    Implement methods for working with skip parser

---
+++ src/commonMain/kotlin/com/github/kkarnauk/parsek/parser/combinators/Skip.kt
@@ -0,0 +1,31 @@
+package com.github.kkarnauk.parsek.parser.combinators
+
+import com.github.kkarnauk.parsek.parser.OrdinaryParser
+import com.github.kkarnauk.parsek.parser.Parser
+import com.github.kkarnauk.parsek.parser.SkipParser
+
+/**
+ * Transforms a parser that uses its result into a parser that ignores it.
+ * The key usage is [seq].
+ */
+public fun <T> skip(parser: OrdinaryParser<T>): SkipParser<T> = SkipParser(parser)
+
+/**
+ * Transforms a parser that uses its result into a parser that ignores it.
+ * The key usage is [seq].
+ */
+public operator fun <T> OrdinaryParser<T>.unaryMinus(): SkipParser<T> = skip(this)
+
+/**
+ * Transforms a parser that ignores its result into a parser that uses it.
+ * The key usage is [seq].
+ */
+public operator fun <T> SkipParser<T>.unaryPlus(): OrdinaryParser<T> = inner
+
+/**
+ * Transforms any parser into a parser that uses its result.
+ */
+public fun <T> Parser<T>.toOrdinary(): OrdinaryParser<T> = when (this) {
+    is OrdinaryParser -> this
+    is SkipParser -> +this
+}
