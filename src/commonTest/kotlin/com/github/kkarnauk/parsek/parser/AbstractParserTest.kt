b3113f7a87db79e51041af4363610c1bbd837965
Kirill <bitikit@gmail.com>
2022-01-29 19:54:05 +0300

    Minor fixes in base class of parser test

--- src/commonTest/kotlin/com/github/kkarnauk/parsek/parser/AbstractParserTest.kt
+++ src/commonTest/kotlin/com/github/kkarnauk/parsek/parser/AbstractParserTest.kt
@@ -8,16 +8,15 @@ import kotlin.properties.Delegates
 import kotlin.test.assertEquals
 
 internal abstract class AbstractParserTest<P : Parser<*>> : ParsekTest() {
-    protected abstract val parser: P
-
     protected fun produceTokens(vararg tokens: TokenDescription): TokenProducer = object : TokenProducer {
-        var last = -1
+        var last = 0
 
         override fun nextToken(): Token? = tokens.getOrNull(last++)?.toToken()
     }
 
     protected inner class InsideTest<R> : AbstractInsideTest() {
         override var text by Delegates.notNull<CharSequence>()
+        var parser by Delegates.notNull<P>()
         var fromIndex = 0
         var tokenProducer by Delegates.notNull<TokenProducer>()
         var expected by Delegates.notNull<ParseResult<R>>()
