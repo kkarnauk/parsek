80e2b4094ccc4b004ba03a03c74160676a878420
Kirill.Karnaukhov <kirill.karnaukhov@jetbrains.com>
2022-02-03 01:55:47 +0300

    Rename char into chars in provider of char predicate token type

--- src/commonMain/kotlin/com/github/kkarnauk/parsek/token/type/provider/CharPredicateTokenTypeProvider.kt
+++ src/commonMain/kotlin/com/github/kkarnauk/parsek/token/type/provider/CharPredicateTokenTypeProvider.kt
@@ -6,7 +6,7 @@ import com.github.kkarnauk.parsek.token.type.CharPredicateTokenType
  * Provides with [CharPredicateTokenType] by a name.
  * Required to take names from properties when constructing a grammar and provide with [CharPredicateTokenType].
  */
-public fun char(
+public fun chars(
     ignored: Boolean = false,
     predicate: (Char) -> Boolean
 ): TokenTypeProvider<CharPredicateTokenType> = object : TokenTypeProvider<CharPredicateTokenType>() {
